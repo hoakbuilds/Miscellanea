92c8c70d2a7f1398eda2950397adafd4fe47d90a
murlocos <hugo.carvalho@ubi.pt>
2018-02-28 17:22:17 +0000

    Big lil update

---
+++ C/linecalc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <errno.h>
+#include <dirent.h>
+
+int main(int argc, char* argv[]){
+	
+	
+	
+	
+	
+	
+	return 0;
+}
