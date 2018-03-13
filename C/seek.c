0f31851938d027f1fff36c9c2c4ccf4427a932c5
murlocos <hugo.carvalho@ubi.pt>
2018-03-13 03:18:14 +0000

    Another one

--- C/seek.c
+++ C/seek.c
@@ -2,29 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <fcntl.h>
 
- /*
- Five types of instructions:
-     (i) go to the beginning or end of file or move forward/backwards n bytes
-     (ii) go forwards/backwards n bytes from the current position 
-     (iii) read n bytes to a buffer
-     (iv) clean the buffer
-     (v) finish
-
-    + n : avan¸car n bytes
-    - n : recuar n bytes
-    i n : ir para o inicio do ficheiro e e avan¸car n bytes
-    f n : ir para o fim do ficheiro e recuar n bytes
-    r n : read/ler n bytes e append a um buffer.
-    l 0 : limpar o buffer
-    s 0 : stop
-*/
-
-void append(char* s, char c) {
-        int len = strlen(s);
-        s[len] = c;
-        s[len+1] = '\0';
+void append(char *buffer, char c){
+    
+    int l = strlen(buffer);
+    buffer[l]=c;
+    buffer[l+1]='\0';
+    c='\0';
 }
 
 int main(int argc, char* argv[]){
@@ -36,47 +23,71 @@ int main(int argc, char* argv[]){
         return 0;
     }
 
-    char c = 0;
-    char r = 0;
-    char buffer[100];
-    __off_t offset = 0;
-    int n = 0, l = 0, i = 0;
+    off_t maxpos = lseek(fd, -1, SEEK_END);
+    off_t minpos = lseek(fd, 0, SEEK_SET);
+    off_t currpos = minpos;
+    int i = 0, n = 0;
+    const char mais = '+';
     const char barran = '\n';
+    const char menos = '-';
+    const char inicio = 'i';
+    const char fim = 'f';
+    const char ler = 'r';
+    const char limpar = 'l';
+    const char stop = 's';
+    char r;
+    char c;
+    char buffer[100];       
 
-    
-    scanf("%c %d", &c, &n);
+    while( 0<scanf("%c %d\n", &c, &n) ){
 
-    while( c != 's' || n != 0){
-        
-        if( c == '+'){
-            offset = lseek(fd, n, SEEK_CUR);
-        }else if( c == '-'){
-            lseek(fd, 0, SEEK_SET);
-            offset = lseek(fd, offset-n, SEEK_CUR);
-        }else if( c == 'i'){
-            lseek(fd, 0, SEEK_SET);
-            offset = lseek(fd, n, SEEK_CUR);
-        }else if( c == 'f'){
-            offset = lseek(fd, 0, SEEK_END);
-            lseek(fd, 0, SEEK_SET);
-            offset = lseek(fd, offset, SEEK_SET);
-        }else if( c == 'r'){
-           for(i=0;i<n;i++){
-                l+=read(fd, &r, 1);
+        if( 0==strncmp(&c, &inicio, 1)  ){
+
+            currpos = lseek(fd, n, SEEK_SET);
+            
+        }else if( 0==strncmp(&c, &menos, 1) ){
+
+            if( ( currpos - n ) < minpos ){
+                n = ( minpos - currpos );
+            }
+
+            currpos = lseek(fd, (-abs(n)), SEEK_CUR);
+                
+        }else if( 0==strncmp(&c, &mais, 1)){
+
+            if( ( currpos + n ) > maxpos ){
+                n = ( maxpos - currpos );
+            }
+                
+            currpos = lseek(fd, n, SEEK_CUR);
+
+        }else if( 0==strncmp(&c, &fim, 1)){
+
+            currpos = lseek(fd, (-abs(n+1)), SEEK_END);
+            
+        }else if( 0==strncmp(&c, &ler, 1)){
+            if(n>99) continue;
+            for(i=0; i<n; i++){
+                read(fd, &r, 1);
                 append(buffer, r);
             }
-        }else if( c == 'l' && n == 0){
-            write(STDOUT_FILENO, &buffer, l);
-            write(STDOUT_FILENO, &barran, 1);
-            printf("%s", buffer);
-            for(i=0;i<100;i++){
-                buffer[i]='\0';
-            }
-        }else if( c == 's' && n == 0){
-            break;
-        }
 
-        scanf("%c %d", &c, &n);
+        }else if( 0==strncmp(&c, &limpar, 1)){
+
+            if( 0 == n ){
+
+                for(i=0;i<100;i++){ 
+                    buffer[i]='\0';
+                }
+            }
+        }else if( 0==strncmp(&c, &stop, 1) ){
+                
+            if( 0 == n ){
+                write(STDOUT_FILENO, buffer, strlen(buffer));
+                write(STDOUT_FILENO, &barran, 1);
+                break;
+            }
+        } else continue;
 
     }
 
