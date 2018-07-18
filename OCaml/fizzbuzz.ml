647f989d151074698a83f3a0fe543111264e3e71
murlocos <hugo.carvalho@ubi.pt>
2018-07-18 11:42:36 +0100

    FizzBuzz!

--- OCaml/fizzbuzz.ml
+++ OCaml/fizzbuzz.ml
@@ -5,5 +5,3 @@ let rec fizzbuzz i =
     if ( i mod 3 = 0 ) && ( i mod 5 = 0 ) then "FizzBuzz" else
     if i mod 5 = 0 then "Buzz" else 
     if i mod 3 = 0 then "Fizz" else string_of_int i ) in fizzbuzz (i+1);;
-
-let a  = fizzbuzz i;;
